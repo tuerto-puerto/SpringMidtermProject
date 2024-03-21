3da2d68e381c6049777066173db6e7730c7739a3
Turat <torotura@icloud.com>
2024-03-21 21:28:11 +0600

    committ

--- src/main/java/turatbek/tor/springproject/repository/StudentRepository.java
+++ src/main/java/turatbek/tor/springproject/repository/StudentRepository.java
@@ -3,6 +3,6 @@ package turatbek.tor.springproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import turatbek.tor.springproject.model.Student;
 
-public interface StudentRepository extends JpaRepository<Student>{
+public interface StudentRepository extends JpaRepository<Student, Long>{
 
 }
