59d687333dc20f677ee1924b7da6b4532b4c71a6
Turat <torotura@icloud.com>
2024-03-21 21:36:12 +0600

    coommit

--- src/main/java/turatbek/tor/springproject/repository/StudentRepository.java
+++ src/main/java/turatbek/tor/springproject/repository/StudentRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import turatbek.tor.springproject.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
+    void deleteByEmail(String email);
+    Student findStudentByEmail(String email);
 
 }
--- src/main/java/turatbek/tor/springproject/service/Impl/StudentServiceImpl.java
+++ src/main/java/turatbek/tor/springproject/service/Impl/StudentServiceImpl.java
@@ -1,28 +1,33 @@
 package turatbek.tor.springproject.service.Impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import turatbek.tor.springproject.model.Student;
+import turatbek.tor.springproject.repository.StudentRepository;
 import turatbek.tor.springproject.service.StudentService;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository repository;
 
 
     @Override
     public List<Student> findAllStudent() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return repository.save(student);
     }
 
     @Override
     public Student findByEmail(String email) {
-        return null;
+        return repository.findStudentByEmail(email);
     }
 
     @Override
@@ -32,6 +37,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public void deleteStudent(String email) {
+        repository.deleteByEmail(email);
 
     }
 }
