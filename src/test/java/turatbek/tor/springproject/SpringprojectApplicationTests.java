25e906fd494626936ce12e80db98746182b38cb5
Turat <torotura@icloud.com>
2024-03-21 15:56:53 +0600

    another

---
+++ src/test/java/turatbek/tor/springproject/SpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package turatbek.tor.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
