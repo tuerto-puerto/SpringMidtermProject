6af97811846bf952ce9a157b30102714dc5b21ba
Turat <torotura@icloud.com>
2024-03-21 21:19:37 +0600

    commit

--- src/main/java/turatbek/tor/springproject/model/Student.java
+++ src/main/java/turatbek/tor/springproject/model/Student.java
@@ -1,15 +1,23 @@
 package turatbek.tor.springproject.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
-@Builder
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
+    @Column(unique = true)
     private String email;
+    @Transient
     private int age;
 }
---
+++ src/main/java/turatbek/tor/springproject/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package turatbek.tor.springproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import turatbek.tor.springproject.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student>{
+
+}
