c88325b52a94e2563e0b71aa80380fed80ed99cb
Turat <torotura@icloud.com>
2024-05-14 06:33:03 +0600

    create Jwt authentication filter

--- src/main/java/turatbek/tor/springproject/service/JwtService.java
+++ src/main/java/turatbek/tor/springproject/service/JwtService.java
@@ -13,6 +13,7 @@ import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
